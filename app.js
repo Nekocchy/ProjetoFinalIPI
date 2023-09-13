cac5070cf0b8a30d34a305e5e9aa2ce783f2ab64
Nekocchy <irisnewdisc10@gmail.com>
2023-09-13 17:06:02 -0300

    Menu Inicial

---
+++ app.js
@@ -0,0 +1,26 @@
+var lista_de_produtos = []
+
+var numero = 5
+
+while (true){
+
+var opcao = prompt (`
+    Bem-vindo ao cadastro de produtos! :D
+    1. Cadastrar novo Produto
+    2. Editar Produto
+    3. Remover Produto
+    4. Visualizar Todos os Produtos 
+    `)
+
+    if(opcao == 1) {
+        // Código para Adicionar Produto
+    }else if(opcao == 2){
+        // Código para Editar Produto
+    }else if(opcao == 3){
+        // Código para Remover Produto
+    }else if(opcao == 4){
+        // Código para Visualizar Produto    
+    }else{
+        alert("Nenhuma das Opções foi Selecionada")
+    }
+}
