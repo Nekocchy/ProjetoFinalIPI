97f394e0d2a13dbf2a2ba9dee166a53daa4a2cd2
NekkoFokkks <94187994+Nekocchy@users.noreply.github.com>
2023-09-20 18:20:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,78 @@
-var lista_de_produtos = []
+var lista = []
 
 var numero = 5
 
 while (true){
 
 var opcao = prompt (`
-    Bem-vindo ao cadastro de produtos! :D
-    1. Cadastrar novo Produto
-    2. Editar Produto
-    3. Remover Produto
-    4. Visualizar Todos os Produtos 
+    Bem-vindo ao cadastro de produtos! 
+    【1️⃣】 Adicionar Novo Produto
+    【2️⃣】 Editar Produto
+    【3️⃣】 Remover Produto
+    【4️⃣】 Visualizar Todos os Produtos 
     `)
 
     if(opcao == 1) {
-        // Código para Adicionar Produto
+        adicionar()
     }else if(opcao == 2){
-        // Código para Editar Produto
+        editar()
     }else if(opcao == 3){
-        // Código para Remover Produto
+        remover()
     }else if(opcao == 4){
-        // Código para Visualizar Produto    
+        ver()    
     }else{
-        alert("Nenhuma das Opções foi Selecionada")
+        alert("« ❗ Nenhuma das Opções foi Selecionada ❗ »")
     }
+}
+
+function adicionar() {
+    var objeto = {
+        nome: '',
+        marca: '',
+        preco: ''
+    }
+
+objeto.nome = prompt ("〚📋〛Escolha o Nome")
+objeto.marca = prompt ("〚📋〛Escolha a Marca")
+objeto.preco = prompt ("〚📋〛Escolha o Preço")
+
+lista.push(objeto)
+
+}
+
+function editar(){
+    var renomear = prompt("Qual produto você quer editar?")
+
+    lista[renomear].nome = prompt ("〚✍️〛Qual o novo nome?")
+    lista[renomear].marca = prompt ("〚✍️〛Qual a nova marca?")
+    lista[renomear].preco = prompt ("〚✍️〛Qual o novo preço?")
+
+    lista.push(renomear)
+
+}
+
+function remover(){
+    var excluir = prompt ("〚🗑️〛Qual item você deseja remover?")
+    lista.splice (excluir, 1)
+
+}
+
+function ver(){
+    var produtos = ""
+
+    for(let i = 0; i < lista.length; i++ ){
+        produtos +=
+        `
+        \n 
+「 NOME 」╎ ${lista[i].nome} 
+        \n
+「 MARCA 」╎ ${lista[i].marca} 
+        \n
+「 PREÇO 」╎ ${lista[i].preco}
+        \n
+◖■ ■ ■ ■ ■ ■ ■ ■◗
+        `
+    }
+
+    alert(produtos)
 }
